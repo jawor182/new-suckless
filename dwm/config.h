20401c1ad343d0d5437e213df020683aa3ad5072
jawor182 <kubajaworski59@outlook.com>
2025-04-27 18:13:41 +0200

    xresources

--- dwm/config.h
+++ dwm/config.h
@@ -1,28 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
-static const char *fonts[]          = { "JetBrainsMonoNerdFont:size=16:antialias=true", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMonoNerdFont:size=16";
-static const char col_gray1[]       = "#1e1e2e";
-static const char col_gray2[]       = "#585b70";
-static const char col_gray3[]       = "#cdd6f4";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_bordersel[]   = "#cba6f7";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_bordersel  },
+static unsigned int borderpx  = 4;        /* border pixel of windows */
+static unsigned int snap      = 32;       /* snap pixel */
+static unsigned int gappih    = 15;       /* horiz inner gap between windows */
+static unsigned int gappiv    = 15;       /* vert inner gap between windows */
+static unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static int showbar            = 1;        /* 0 means no bar */
+static int topbar             = 1;        /* 0 means bottom bar */
+static int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+static char font[]            = { "JetBrainsMonoNerdFont:size=16:antialias=true"};
+static char dmenufont[]       = "JetBrainsMonoNerdFont:size=16";
+static const char *fonts[]          = { font };
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 #define TERMINAL "st"
@@ -41,10 +42,10 @@ static const Rule rules[] = {
 
 
 /* layout(s) */
-static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
+static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
@@ -83,7 +84,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]        = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]        = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]         = { TERMINAL, NULL };
 static const char *browser[]         = { "brave", NULL };
 static const char *email[]           = { "thunderbird", NULL };
@@ -102,7 +103,30 @@ static const char *btsettintgs[]     = { TERMINAL, "-e", "bluetuith", NULL };
 static const char *passwords[]       = { "keepassxc", NULL };
 static const char *screenshot[]      = { "flameshot", "gui", NULL };
 static const char *wallpaper[]       = { "nitrogen", NULL };
+static const char *randomWallpaper[] = { "sh", "-c", "setbg $HOME/walls", NULL };
+static const char *communicator[]    = { "discord", NULL };
 static const char *lockscreen[]      = { "xsecurelock", NULL };
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+        { "dwm.font", STRING, &font },
+        { "dmenu.font", STRING, &dmenufont },
+		{ "normbgcolor",        STRING,  &normbgcolor },
+		{ "normbordercolor",    STRING,  &normbordercolor },
+		{ "normfgcolor",        STRING,  &normfgcolor },
+		{ "selbgcolor",         STRING,  &selbgcolor },
+		{ "selbordercolor",     STRING,  &selbordercolor },
+		{ "selfgcolor",         STRING,  &selfgcolor },
+		{ "borderpx",          	INTEGER, &borderpx },
+		{ "snap",          		INTEGER, &snap },
+		{ "showbar",          	INTEGER, &showbar },
+		{ "topbar",          	INTEGER, &topbar },
+		{ "nmaster",          	INTEGER, &nmaster },
+		{ "resizehints",       	INTEGER, &resizehints },
+		{ "mfact",      	 	FLOAT,   &mfact },
+};
 
 
 static const Key keys[] = {
@@ -164,11 +188,13 @@ static const Key keys[] = {
     { MODKEY,                       XK_m,      spawn,          {.v = music } },
     { MODKEY,                       XK_n,      spawn,          {.v = notes } },
     { MODKEY,                       XK_f,      spawn,          {.v = fileManager } },
+    { MODKEY,                       XK_c,      spawn,          {.v = communicator } },
     { MODKEY|Mod1Mask,              XK_f,      spawn,          {.v = guiFileManager } },
     { MODKEY|ControlMask,           XK_s,      spawn,          {.v = audiosettintgs } },
     { MODKEY|ControlMask,           XK_b,      spawn,          {.v = btsettintgs } },
     { MODKEY,                       XK_p,      spawn,          {.v = passwords } },
-    { MODKEY|ControlMask,           XK_w,      spawn,          {.v = wallpaper } },
+    { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = wallpaper } },
+    { MODKEY|ControlMask,           XK_w,      spawn,          {.v = randomWallpaper } },
     { MODKEY|ShiftMask,             XK_s,  spawn,              {.v = screenshot } },
     { MODKEY,                       XK_Print,  spawn,          {.v = screenshot } },
 	TAGKEYS(                        XK_1,                      0)
