c90d038a5bb48b8e620cedc47c330647f0d8af6a
jawor182 <kubajaworski59@outlook.com>
2025-08-16 13:06:51 +0200

    rule for mpv

--- dwm/config.h
+++ dwm/config.h
@@ -37,7 +37,7 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
     /* class                         instance       title               tags mask  isfloating  isterminal  noswallow  monitor  scratchkey*/
-    { "Gimp",                        NULL,          NULL,               0,         0,          0,          0,         -1,       0   },
+    { "mpv",                         NULL,          NULL,               0,         1,          0,          0,          1,       0   },
     { "KeePassXC",                   NULL,          NULL,               1 << 8,    0,          0,          0,          0,       0   }, // workspace 9, monitor 1
     { "discord",                     NULL,          NULL,               1 << 3,    0,          0,          0,          1,       0   }, // workspace 4, monitor 2
     { "thunderbird",                 NULL,          NULL,               1 << 2,    0,          0,          0,          0,       0   }, // workspace 4, monitor 1
